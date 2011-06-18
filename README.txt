69ff4b0aad9e3f3af60c6dbb10701476a4cac5f7
Maik R��der <maikroeder@gmail.com>
2011-06-18 22:22:48 +0000

    move

---
+++ README.txt
@@ -0,0 +1,13 @@
+= rnaseq.restkit =
+
+This module makes use of the restkit library to fetch resources from the restish server.
+
+For further information on rest, check out the homepage:
+
+    http://benoitc.github.com/restkit/
+    
+The only thing you need to know about this module is that is should reliably deliver
+resources fetched from a remote server.
+
+The rnaseq.restyler module uses this module heavily to fetch all the resources for the
+boxes it renders.
---
+++ rnaseq/restkit/__init__.py
@@ -0,0 +1,65 @@
+import restkit
+from restkit import request
+from restkit.errors import ResourceNotFound
+from restkit.errors import Unauthorized
+from restkit.errors import RequestFailed
+from restkit.errors import RedirectLimit
+from restkit.errors import RequestError
+from restkit.errors import InvalidUrl
+from restkit.errors import ResponseError
+from restkit.errors import ProxyError
+from restkit.errors import BadStatusLine
+from restkit.errors import ParserError
+from restkit.errors import UnexpectedEOF
+
+#restkit.set_logging('debug')
+
+class BaseResource():
+    def _get(self, path, content_type=None):
+        if " " in path:
+            raise AttributeError
+        if content_type:
+            headers= {'Accept': content_type}
+        else:
+            headers = {}
+        try:
+            res = request(path, headers = headers)
+        except (ResourceNotFound, 
+               Unauthorized, 
+               RequestFailed, 
+               RedirectLimit,
+               RequestError,
+               InvalidUrl,
+               ResponseError,
+               ProxyError,
+               BadStatusLine,
+               ParserError,
+               UnexpectedEOF), e:
+            print path
+            return None
+        except:
+            raise
+        if res is None:
+            return None
+        body = None
+        if res.status == '200 OK':
+            body = res.body_string()
+            if res.headers.has_key('Content-Length'):
+                content_length = res.headers['Content-Length']
+            elif res.headers.has_key('content-length'):
+                content_length = res.headers['content-length']
+            else:
+                print "Warning: Content length header not found!"
+                raise AttibuteError
+            if not len(body) == int(content_length):
+                print "Warning: Body length not correct!"
+                raise AttributeError
+        return body
+
+class GenericResource(BaseResource):
+        
+    def get(self, path, content_type=None):
+        print path, content_type
+        return self._get(path, content_type)
+
+resource = GenericResource()
